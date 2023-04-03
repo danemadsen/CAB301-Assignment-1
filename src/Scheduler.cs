b607c9acaf2c712da2c7419865fda8fa054ab981
Dane Madsen <dane_madsen@hotmail.com>
2023-04-04 08:43:57 +1000

    Fixes

--- src/Scheduler.cs
+++ src/Scheduler.cs
@@ -7,56 +7,62 @@ public class Scheduler : IScheduler {
 
 	public IJobCollection Jobs { get; }
 
+// Selection sort
 public IJob[] FirstComeFirstServed()
 {
     IJob[] sortedJobs = Jobs.ToArray();
-
     for (int i = 0; i < sortedJobs.Length - 1; i++)
     {
-        for (int j = i + 1; j > 0; j--)
+        for (int j = i + 1; j < sortedJobs.Length; j++)
         {
-            if (sortedJobs[j].TimeReceived < sortedJobs[j - 1].TimeReceived)
+            if (sortedJobs[i].TimeReceived > sortedJobs[j].TimeReceived)
+            {
+                IJob temp = sortedJobs[i];
+                sortedJobs[i] = sortedJobs[j];
+                sortedJobs[j] = temp;
+            }
+        }
+    }
+    return sortedJobs;
+}
+
+// Insertion Sort
+public IJob[] Priority()
+{
+    IJob[] sortedJobs = Jobs.ToArray();
+    for (int i = 1; i < sortedJobs.Length; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (sortedJobs[j].Priority < sortedJobs[j - 1].Priority)
             {
                 IJob temp = sortedJobs[j];
                 sortedJobs[j] = sortedJobs[j - 1];
                 sortedJobs[j - 1] = temp;
             }
-        }
-    }
-    return sortedJobs;
-}
-
-
-public IJob[] Priority()
-{
-    IJob[] sortedJobs = Jobs.ToArray();
-    for (int i = 0; i < sortedJobs.Length - 1; i++)
-    {
-        for (int j = i + 1; j < sortedJobs.Length; j++)
-        {
-            if (sortedJobs[i].Priority > sortedJobs[j].Priority)
+            else
             {
-                IJob temp = sortedJobs[i];
-                sortedJobs[i] = sortedJobs[j];
-                sortedJobs[j] = temp;
+                break;
             }
         }
     }
     return sortedJobs;
 }
 
+// Bubble sort
 public IJob[] ShortestJobFirst()
 {
     IJob[] sortedJobs = Jobs.ToArray();
-    for (int i = 0; i < sortedJobs.Length - 1; i++)
+    int n = sortedJobs.Length;
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i + 1; j < sortedJobs.Length; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
-            if (sortedJobs[i].ExecutionTime > sortedJobs[j].ExecutionTime)
+            if (sortedJobs[j].ExecutionTime > sortedJobs[j + 1].ExecutionTime)
             {
-                IJob temp = sortedJobs[i];
-                sortedJobs[i] = sortedJobs[j];
-                sortedJobs[j] = temp;
+                IJob temp = sortedJobs[j];
+                sortedJobs[j] = sortedJobs[j + 1];
+                sortedJobs[j + 1] = temp;
             }
         }
     }
